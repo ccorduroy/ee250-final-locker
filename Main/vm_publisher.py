3fe6d687fd73d0e5b728b771692fc310da7d3057
Caitlin Sullivan <ccsulliv@usc.edu>
2024-11-26 16:05:06 -0800

    remove LCD/USR, add pot

--- Main/vm_publisher.py
+++ Main/vm_publisher.py
@@ -8,50 +8,17 @@ from pynput import keyboard
 import threading
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+    print("Connected to broker with result code "+str(rc))
     #subscribe to topics of interest here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
-# parallel task/thread to read keyboard input
-def kbd_thread():
-        while True:
-            # must hit enter to complete the input
-            k = input("")
-            if k == 'w':
-                print("w")
-                # send "w" character to rpi
-                client.publish("samardzi/lcd", "w")
-            elif k == 'a':
-                print("a")
-                # send "a" character to rpi
-                # subscribe
-                client.publish("samardzi/led", "LED_ON")
-                client.publish("samardzi/lcd", "a")
-                # send "LED_ON"
-            elif k == 's':
-                print("s")
-                # send "s" character to rpi
-                client.publish("samardzi/lcd", "s")
-            elif k == 'd':
-                print("d")
-                # send "d" character to rpi
-                # send "LED_OFF"
-                client.publish("samardzi/led", "LED_OFF")
-                client.publish("samardzi/lcd", "d")
-
-
 if __name__ == '__main__':
     #setup the keyboard event listener
     #lis = keyboard.Listener(on_press=on_press)
     #lis.start() # start to listen on a separate thread
-
-    thread = threading.Thread(target=kbd_thread)
-    #thread.daemon = True
-    # start the thread executing
-    thread.start()
 
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
