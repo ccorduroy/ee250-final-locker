b73fb837be3bf76101fac7d1b0751fac0977155f
Caitlin Sullivan <ccsulliv@usc.edu>
2024-11-28 15:11:44 -0800

    .

--- Main/rpi_pub_and_sub.py
+++ Main/rpi_pub_and_sub.py
@@ -14,6 +14,14 @@ potentiometer = 0 #A0
 grovepi.pinMode(button, "INPUT")
 grovepi.pinMode(potentiometer, "INPUT")
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server (i.e., broker) with result code " + str(rc))
+
+    #subscribe to topics of interest here
+    #client.subscribe("samardzi/led")    # add back callback
+    #client.message_callback_add("samardzi/led", led_callback )
+
+    #print("subscribed to LED, LCD")
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
@@ -30,9 +38,11 @@ def callback_pot(client, userdata, msg):
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
-    #this section is covered in publisher_and_subscriber_example.py
+    # this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
     client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="broker.emqx.io", port=1883, keepalive=60)
     client.loop_start()
 
     temp = 0
